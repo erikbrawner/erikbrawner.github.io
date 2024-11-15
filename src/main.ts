416ad284199f8f275e20996ac190da5d98f477e1
Erik Brawner <evbrawner@gmail.com>
2024-11-15 07:09:16 -0600

    show and hide ui icon

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 document.getElementById('hideInfo-btn')!.addEventListener('click', () => {
     let scrollDiv = document.getElementById('scroll-div')! as HTMLElement;
     let infoDivs = document.querySelectorAll('.info-div')! as NodeListOf<HTMLElement>;
+    let hideInfoIcon = document.getElementById('hideInfo-svg')! as HTMLElement;
+    let showInfoIcon = document.getElementById('showInfo-svg')! as HTMLElement;
     
     /*for (var i = 0; i < infoDivs.length; i++) {
         if (infoDivs[i].style.opacity == '0') {
@@ -18,11 +20,15 @@ document.getElementById('hideInfo-btn')!.addEventListener('click', () => {
     if (scrollDiv.style.opacity == '0') {
         scrollDiv.style.opacity = '1';
         scrollDiv.style.pointerEvents = 'auto';
-        document.getElementById('hideInfo-btn')!.style.background = "url('./src/assets/eye.svg')";
+        //document.getElementById('hideInfo-btn')!.style.background = "url('./src/assets/eye.svg')";
+        hideInfoIcon.classList.remove('hidden');
+        showInfoIcon.classList.add('hidden');
     } else {
         scrollDiv.style.opacity = '0';
         scrollDiv.style.pointerEvents = 'none';
-        document.getElementById('hideInfo-btn')!.style.background = "url('./src/assets/eye-off.svg')";
+        //document.getElementById('hideInfo-btn')!.style.background = "url('./src/assets/eye-off.svg')";
+        hideInfoIcon.classList.add('hidden');
+        showInfoIcon.classList.remove('hidden');
     }
 });
 
@@ -30,6 +36,6 @@ document.getElementById('scrollIndicator-img')!.addEventListener('click', () => 
     document.getElementById('about-div')!.scrollIntoView({behavior: 'smooth'});
 });
 
-document.getElementById('hideInfo-btn')!.addEventListener('domContentLoaded', () => {
+/*document.getElementById('hideInfo-btn')!.addEventListener('domContentLoaded', () => {
     document.getElementById('hideInfo-btn')!.style.background = "url('./src/assets/eye.svg')";
-});
+});*/
